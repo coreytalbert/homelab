58845d8aa4f7d214509a24eb9072ceefa295ec4e
Corey Talbert <coreytalbert@gmail.com>
2024-05-29 00:09:31 -0400

    homelab_init edits

--- homelab_init.ps1
+++ homelab_init.ps1
@@ -7,22 +7,24 @@ $CPU_count = 2
 $image_path = "..."
 
 # https://learn.microsoft.com/en-us/windows-server/virtualization/hyper-v/get-started/create-a-virtual-switch-for-hyper-v-virtual-machines?tabs=powershell
-$switch_params = @{
-    Name           = "switch0"
-    NetAdapterName = $(Get-NetAdapter | Where-Object Name -Like "*Wi-Fi*")
-}
+#$switch_params = @{
+#    Name           = "switch0"
+#    NetAdapterName = $(Get-NetAdapter | Where-Object Name -Like "*Wi-Fi*")
+#}
+#New-VMSwitch @switch_params | Tee-Object -FilePath "./homelab_init.log"
 
-New-VMSwitch @switch_params | Tee-Object -FilePath "./homelab_init.log"
+$switch_name = Get-VMSwitch -name "vWifi" | select -expand Name
 
 ForEach ($vm_name in $vm_names) {
     # https://learn.microsoft.com/en-us/windows-server/virtualization/hyper-v/best-practices-for-running-linux-on-hyper-v
+    Write-Host "Creating VM $vm_name"
     
     $vm_params = @{
         Name               = $vm_name
         MemoryStartupBytes = $startup_memory
         Generation         = 2
         Path               = "C:\Virtual Machines\$vm_name"
-        SwitchName         = $switch_params['Name']
+        SwitchName         = $switch_name
     }
 
     $vm = New-VM @vm_params 
@@ -35,7 +37,7 @@ ForEach ($vm_name in $vm_names) {
 
     New-VHD @vhd_params -Dynamic
 
-    $vm | Set-VHD -Path $vhd_params['Path']
+    $vm | Add-VMHardDiskDrive -Path $vhd_params['Path']
 
     $vm | Set-VMMemory -DynamicMemoryEnabled $true -MinimumBytes $minimum_memory -MaximumBytes $maximum_memory
 
